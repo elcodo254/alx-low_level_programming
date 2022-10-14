8aea158d07cd4c086e3cc59932535c23450a79fe
elcodo254 <ellawachaga@gmail.com>
2022-10-14 14:50:10 -0700

    c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/*
+ * Main - Entry point
+ * char -string initialisation
+ * puts -string to stdout
+ * return -always 0 (success)
+ */
+
+init main(void)
+
+{
+
+	char strng[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(strng);
+
+	return (0);
+
+}
