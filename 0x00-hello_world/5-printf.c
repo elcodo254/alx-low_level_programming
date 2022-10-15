1806d4fcd3abc9995be1dfc71febc3b73f1a887f
elcodo254 <ellawachaga@gmail.com>
2022-10-14 18:04:05 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ * Return: 0 if successful
+ */
+
+int main(void)
+
+{
+
+	printf("%s \n", "with proper grammar, but the outcome is a piece of art,");
+
+	return 0;
+
+}
