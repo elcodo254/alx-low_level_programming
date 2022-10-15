d8c666bbfe35017d6fa05f7f28ce42d3f5ac7584
elcodo254 <ellawachaga@gmail.com>
2022-10-14 19:48:48 -0700

    replace zu with ld

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,15 +18,15 @@ int main(void)
 
 	float floatType;
 
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of char: %ld byte(s)\n", sizeof(charType));
 
-	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
 
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
 
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
 
-	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 
 	return (0);
 
