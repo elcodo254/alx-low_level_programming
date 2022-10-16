302b3d19e970819f6dc4862ba99f898976a077f9
elcodo254 <ellawachaga@gmail.com>
2022-10-16 16:28:43 -0700

    last digit and print conditionally

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int ldgt = 0;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	ldgt = n % 10;
+if (ldgt > 5)
+{
+	printf("Last digit of %i is %i and is greater than 5\n", n, ldgt);
+}
+else if (ldgt == 0)
+{
+	printf("Last digit of %i is %i and is 0\n", n, ldgt);
+}
+if (ldgt < 6 && ldgt != 0)
+{
+	printf("Last digit of %i is %i and is less than 6\n", n, ldgt);
+}
+return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-0-positive_or_negative.c: Print whether variable is positive or negative
+0-positive_or_negative.c: Print whether variable is positive or negative/n1-last_digit.c: Pick last digit of variable and print conditionally
