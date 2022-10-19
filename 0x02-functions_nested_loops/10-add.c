6f0d68f62d6b88be37c9361dd992196aeadbd0e6
elcodo254 <ellawachaga@gmail.com>
2022-10-19 16:34:54 -0700

    clean code for addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return: the result of addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
