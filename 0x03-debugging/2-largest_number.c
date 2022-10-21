12b39121ec83ad6bf6f4677f2f2fbbcd02a19924
elcodo254 <ellawachaga@gmail.com>
2022-10-20 18:57:45 -0700

    clean code

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,13 +17,9 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	if (b > a && b > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
-	}
-	if (c > a && c > b)
-	{
-		largest = c;
 	}
 
 	return (largest);
