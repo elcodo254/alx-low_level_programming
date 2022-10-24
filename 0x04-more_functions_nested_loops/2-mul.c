102100b1b7811aa51eacfcc0096e7e6adcf19acb
elcodo254 <ellawachaga@gmail.com>
2022-10-23 19:45:07 -0700

    code + readme

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: results of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0-isupper.c: Check for uppercase character
 1-isdigit.c: Check for digit between 0 and 9
+2-mul.c: Multiply two numbers
