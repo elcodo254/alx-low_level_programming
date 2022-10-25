479ca691a31ef76e44d5755fdf8dbfc33118d3ad
elcodo254 <ellawachaga@gmail.com>
2022-10-25 16:38:58 -0700

    code + readme + directory

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset pointer value to 98
+ * @n: input to be saved
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-reset_to_98.c: function that takes pointer to int as parameter and updates the value
