72b3d83f047dd51d96a0a50e966d02e8ac7c14b5
elcodo254 <ellawachaga@gmail.com>
2022-10-29 06:25:02 -0700

    code + readme

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lower case to upper
+ * @str: string to be changed
+ * Return: pointer to changed string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[index])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,4 +2,5 @@
 1-strncat.c: concatenate two strings using strncat
 2-strncpy.c: copy one string to another
 3-strcmp.c: compare two strings
-4-rev_array.c: reverse an array 
+4-rev_array.c: reverse an array
+5-string_toupper.c: change lowercase letters to uppercase 
