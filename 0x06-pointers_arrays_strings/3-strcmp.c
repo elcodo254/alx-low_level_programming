87865ac37a3f43cce5f250105696ccb1d65bbee0
elcodo254 <ellawachaga@gmail.com>
2022-10-28 20:43:26 -0700

    code

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ *@s1: first string
+ *@s2: second string
+ *Return: 0 if contains same string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int index;
+
+	for (index = 0; s1[index]; index++)
+	{
+	}
+	for (index = 0; s2[index]; index++)
+	{
+	}
+	if (s2[index] == s1[index])
+		return (0);
+	else
+		return (s2[index] - si[index]);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0x06-pointers_arrays_strings: cancatenate two strings
 1-strncat.c: concatenate two strings using strncat
 2-strncpy.c: copy one string to another
+3-strcmp.c: compare two strings 
