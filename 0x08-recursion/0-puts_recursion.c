2a2184d5d7a867fd25e03b6b91a30693fb1dfcf5
elcodo254 <ellawachaga@gmail.com>
2022-11-03 10:21:38 -0700

    correct code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,14 +3,15 @@
 /**
  * _puts_recursion - print string followed by new line
  * @s: string to be printed
- * Return: 0 success
  */
 void _puts_recursion(char *s)
 {
-
-
+	if (*s)
+	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
+	}
+	else
 		_putchar('\n');
 
 }
