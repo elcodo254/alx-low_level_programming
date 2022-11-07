145998fc5bd6989069249ad5e899aeb0db1e6402
elcodo254 <ellawachaga@gmail.com>
2022-11-07 10:58:53 -0800

    code

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints arguments received
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0, success
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,3 @@
 0-whatsmyname.c: print name of program
 1-args.c: prints number of arguments
+1-args.c: prints all arguments received
