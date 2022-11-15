b63076bcc5f0e2788c262ab12e726a4fc59d6563
elcodo254 <ellawachaga@gmail.com>
2022-11-15 12:14:54 -0800

    code+readme+headerfile

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes variable of type struct dog
+ * @d: dog to be initialized
+ * @name: name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d == 0)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+dog.h: header file
+1-init_dog.c: initialize a variable of type struct dog
+2-print_dog.c: Print a struct dog
+4-new_dog.c: Creates a new dog with stored name of dog and owner
+5-free_dog.c: Free dog
+
