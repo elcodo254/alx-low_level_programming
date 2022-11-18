d8e18c7b27ddef819bf6a9020ef9764b8be4413b
elcodo254 <ellawachaga@gmail.com>
2022-11-17 18:37:07 -0800

    code

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opcode of its own main function
+ * @arc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int n_bytes = 0;
+	int i;
+	void *magic_ptr = NULL;
+	char *final_ptr = NULL;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n_bytes = atoi(argv[1]);
+
+	if (n_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	magic_ptr = main;
+	final_ptr = magic_ptr;
+
+	for (i = 0; i < n_bytes; i++)
+	{
+		printf("%02x", final_ptr[i] & 0xFF);
+		if (i < n_bytes - 1)
+			putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
