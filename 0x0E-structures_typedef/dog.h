16a47101b6503d19458ee1b3336ba7935f4ce275
elcodo254 <ellawachaga@gmail.com>
2022-11-21 17:55:55 -0800

    tryt

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog - dog structure
+ * struct dog - structure with details on dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
@@ -12,13 +12,6 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}
-dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *, float, char *);
-int _strlen(char *);
-void free_dog(dog_t *);
+};
 
 #endif/*DOG_H*/
