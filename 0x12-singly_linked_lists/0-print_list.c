1f678edcc71a8a78dfc075d421368577ea40bc66
elcodo254 <ellawachaga@gmail.com>
2022-11-29 13:57:40 -0800

    debug

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,8 +12,8 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)");
+		if (h->str == NULL)
+			printf("%u] %s\n", '0', "(nil)");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
