e469e89199881c3a0d33a2f47b7ce32ba2012920
elcodo254 <ellawachaga@gmail.com>
2022-11-29 11:59:32 -0800

    debug

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,11 @@
 
 /**
  * print_list - prints all elements of a list
- * @h: list list_t
+ * @h: list
  *
  * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
@@ -14,8 +15,10 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
 		nodes++;
 		h = h->next;
 	}
