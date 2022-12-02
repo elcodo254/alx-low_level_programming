0200fc1bc112046fdb799e6dce77cbe1dbfa5723
elcodo254 <ellawachaga@gmail.com>
2022-12-02 09:43:11 -0800

    clean code

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,18 +9,13 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	int i = 0;
-	listint_t *tempnode;
+	unsigned int i;
 
-	if (!index)
-		return (0);
-
-	while (head->next != NULL && i <= index)
+	for (i = 0; i < index, i++)
 	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
-		i++;
 	}
-	tempnode = head->next;
-	head->next = tempnode->next;
-	return (tempnode);
+	return (head);
 }
