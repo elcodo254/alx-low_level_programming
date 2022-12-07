0329452e470e3e06f4403f60dd98aa1fe0b26320
elcodo254 <ellawachaga@gmail.com>
2022-12-06 18:42:59 -0800

    clean code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b || (b[i] == '\0'))
 		return (0);
-	while (b[i] == '0' || b[i] == '1')
+	while ((b[i] == '0') || (b[i] == '1'))
 	{
 		dgts <<= 1;
 		dgts += b[i] - '0';
