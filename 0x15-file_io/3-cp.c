f015e3a0d90cad16b7e7efa3a288f3aef9089b04
elcodo254 <ellawachaga@gmail.com>
2022-12-11 15:59:11 -0800

    debugg

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
