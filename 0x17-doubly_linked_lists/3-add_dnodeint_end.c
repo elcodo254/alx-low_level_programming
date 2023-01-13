5ff3076d92c6f3535c5eae675026dfabf6c64852
elcodo254 <ellawachaga@gmail.com>
2023-01-12 18:08:43 -0800

    debug

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,7 +10,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *tmp;
 
-	new = malloc(sizeof(dlisint_t));
+	new = malloc(sizeof(dlistint_t));
 	if (new)
 		return (NULL);
 
