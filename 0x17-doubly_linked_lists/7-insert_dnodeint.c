ffe98e29d837060dd0e2cf4294f27bea8f4ab6d3
elcodo254 <ellawachaga@gmail.com>
2023-01-12 18:51:37 -0800

    code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,6 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL || h == NULL)
 		return (NULL);
+
 	new->n = n;
 	tmp = *h;
 
